46df95fee303ee36bff4d7c7a71fce4a3bd18f39
Ijeoma Onuaju <ijeomaonuajurx@gmail.com>
2023-09-15 13:17:05 +0100

    message

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -2,7 +2,10 @@ import styles from '../styles/faq.module.css'
 
 const Faq = () => {
     return (
-        <h1 className={styles.title}>Easpay</h1>
+        <div className={styles.FaqDiv}>
+            <h4>FAQs</h4>
+            <p>Are you looking for a reliable payment processor to help you expand your online business? Look no further, Zenithpay is here to help you access a larger market with ease!</p>
+        </div>
     )
 }
 export default Faq;
