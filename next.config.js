fe04ca4a421394dff6196228e8218e905e959154
Ijeoma Onuaju <ijeomaonuajurx@gmail.com>
2023-09-18 14:32:55 +0100

    message

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: {
-    loader: 'custom',
-    loaderFile: './my-loader.ts',
-  },
+  // output: 'export',
+  
 }
 
 
